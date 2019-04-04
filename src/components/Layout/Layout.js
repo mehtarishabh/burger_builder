e523c41bd8af627cecabc91f31c256c6b0e1588a
YML <yml@Vivian-MBP.local>
2019-04-04 17:17:50 +0530

    side Bar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,33 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
 
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => {
-    return (
-        <Fragment>
-        <Toolbar />
-        <main className={classes.Content}>{props.children}</main>
-        </Fragment>
-    );
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        })
+    }
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        })
+    }
+    render() {
+        return (
+            <Fragment>
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+            <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+            <main className={classes.Content}>{this.props.children}</main>
+            </Fragment>
+        );
+    }
 }
 
-export default layout;
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,16 +3,19 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => {
     return (
-            <header className={classes.Toolbar}>
-                <div>MENU</div>
-                <Logo />
-                <nav>
-                    <NavigationItems ></NavigationItems>
-                </nav>
-            </header>
+        <header className={classes.Toolbar}>
+            <DrawerToggle clicked={props.drawerToggleClicked}/>
+            <div className={classes.Logo}>
+                <Logo/>
+            </div>
+            <nav className={classes.DesktopOnly}>
+                <NavigationItems ></NavigationItems>
+            </nav>
+        </header>
     )
 }
 
