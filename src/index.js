9eb16638f8bcaf93c2394154f9e8eb1d6852256a
Rishabh Mehta <mehta.rishabh0@gmail.com>
2019-04-10 11:09:50 +0530

    middleware startup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 
 import './index.css';
 import App from './App';
@@ -12,11 +12,16 @@ import reducer from './store/reducer';
 // Creating Middleware
 const logger = store => {
     return next => {
-        
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        }
     }
 }
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(logger));
 
 const app = (
     <Provider store={store}>
